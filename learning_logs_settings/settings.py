2e903e7306759ee5107d0dce406e79c3bfc7437e
Carbon Liu <carbon_20140425@163.com>
2026-07-04 16:53:09 +0800

    Modify settings for production environment
    Updated SECRET_KEY and ALLOWED_HOSTS for production.

--- learning_logs_settings/settings.py
+++ learning_logs_settings/settings.py
@@ -21,11 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "os.environ['SECRET_KEY']"
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ['CarbonChemistry.pythonanywhere.com']
 
 
 # Application definition
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     # Local apps
     'learning_logs_app',
     'user_app',
-    
+
     # Third-party apps
     "bootstrap4",
 ]
